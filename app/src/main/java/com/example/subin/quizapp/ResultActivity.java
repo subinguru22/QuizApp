887e5827eada3e53e94a03d9312cca1e8d935f39
Subin <subin.g22@gmail.com>
2018-01-19 09:04:11 +0530

    Scores activity, highscore store in shared preferences

--- app/src/main/java/com/example/subin/quizapp/ResultActivity.java
+++ app/src/main/java/com/example/subin/quizapp/ResultActivity.java
@@ -1,13 +1,80 @@
 package com.example.subin.quizapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView categoryTV, scoreTV, highScoreTV;
+    Button finishButton;
+    private ProgressDialog progressBar;
+
+    public final static String Message = "com.example.subin.quizapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        categoryTV = (TextView) findViewById(R.id.categoryTV);
+        scoreTV = (TextView) findViewById(R.id.scoreTV);
+        highScoreTV = (TextView) findViewById(R.id.highScoreTV);
+        finishButton = (Button) findViewById(R.id.finishButton);
+
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category"); //receive string message from previous intent
+        int score = intent.getIntExtra("score", 0);
+        scoreTV.setText("Your score: " + score);
+
+        //use Shared preferences for storing and retrieving high score
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        int highScore = sharedPreferences.getInt("highScore",0);
+        if(highScore > score){
+            highScoreTV.setText("High Score: " + highScore);
+        } else {
+            highScoreTV.setText("New High Score: " + score);
+            SharedPreferences.Editor spEditor = sharedPreferences.edit();
+            spEditor.putInt("highScore", score);
+            spEditor.commit();
+        }
+
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //To show button click
+                new Handler().postDelayed(new Runnable() {@Override public void run(){}}, 400);
+                progressBar = new ProgressDialog(view.getContext());//Create new object of progress bar type
+                progressBar.setCancelable(false);//Progress bar cannot be cancelled by pressing any wher on screen
+                progressBar.setMessage("Thank you...");//Title shown in the progress bar
+                progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);//Style of the progress bar
+                progressBar.setProgress(0);//attributes
+                progressBar.setMax(100);//attributes
+                progressBar.show();//show the progress bar
+                //This handler will add a delay of 2 seconds
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Intent start to open the navigation drawer activity
+                        progressBar.cancel();//Progress bar will be cancelled (hide from screen) when this run function will execute after 3.5seconds
+                        Intent intent=new Intent(getApplicationContext(),MenuScreen.class);
+                        intent.putExtra(Message, "Computers");//by this statement we are sending the name of the button that invoked the new Questions.java activity "Message" is defined by the name of the package + MESSAGE
+                        startActivity(intent);
+                    }
+                }, 2000);
+            }
+        });
     }
+
+
+
 }
